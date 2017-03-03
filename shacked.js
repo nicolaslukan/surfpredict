875ba76b96d1cc369095a602d1312f67119bcf5b
Nicolas Lukan <nicolaslukan@students.berkeley.net>
2017-03-03 11:33:14 -0800

    CUFF

--- shacked.js
+++ shacked.js
@@ -2,7 +2,14 @@
  * Created by h205p3 on 2/1/17.
  */
 
-function Spot(height,wind,direction,gnar){
+$(document).ready(function(){
+    $('button').click(function(){
+        $("#homeslice").fadeOut()
+    });
+});
+
+function Spot(name,height,wind,direction,gnar){
+    this.name = name
     this.height = height
     this.wind = wind
     this.direction = direction
@@ -44,8 +51,8 @@ function Location(){
 function board(){
     var ability = document.getElementById("ability").value
     var chiefed = document.getElementById("chiefed").value
-    var spot1= new Spot(4,9,"onshore",2)
-    var spot2= new Spot(10,3,"offshore",8)
+    var spot1= new Spot("pacifica",4,9,"onshore",2)
+    var spot2= new Spot("OB",10,3,"offshore",8)
     var surfer1 = new Surfer(ability,chiefed)
     var quiver1 = new Quiver(8.5,"longboard",1)
     var quiver2 = new Quiver(6,"shortboard",3)
@@ -56,8 +63,16 @@ function board(){
     console.log(surfer1)
 }
 
+function chooseSurfer(){
+
+}
+
+function chooseBoard(){
+
+}
+
 //FIND A WAY TO FACTOR IN TIDE
 //Ability in 3 categories = beginner,intermediate,expert
 //board types in 2 categories = shortboard,longboard
 //direction types in 3 categories = offshore,onshore,sideshore
-//chiefed types in 3 categories = sober, half-baked, zooted.
+//chiefed types in 3 categories = sober, half-baked, zooted
