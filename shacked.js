870668bf7c339889a0f70828f79ba25818263600
Nicolas Lukan <nicolaslukan@students.berkeley.net>
2017-03-14 10:46:52 -0700

    CUFF

--- shacked.js
+++ shacked.js
@@ -18,7 +18,6 @@ function Spot(name,height,wind,direction,gnar){
 
 function Surfer(ability,chiefed){
     this.ability = ability
-    this.chiefed = chiefed
     this.surfboard = [];
 
     this.addBoard = function(quiver){
@@ -33,11 +32,10 @@ function Quiver(height,type,numFins){
 }
 function Location(){
     var ability = document.getElementById("ability").value
-    var chiefed = document.getElementById("chiefed").value
     var spot1= new Spot(4,9,"onshore",2)
     var spot2= new Spot(10,3,"offshore",8)
-    var surfer1 = new Surfer(ability,chiefed)
-    var surfer2 = new Surfer("expert","sober")
+    var surfer1 = new Surfer(ability)
+    var surfer2 = new Surfer("expert",)
     var quiver1 = new Quiver(8.5,"longboard",1)
     var quiver2 = new Quiver(6,"shortboard",3)
 
@@ -48,7 +46,7 @@ function Location(){
 
 }
 
-function board(){
+function examples(){
     var ability = document.getElementById("ability").value
     var chiefed = document.getElementById("chiefed").value
     var spot1= new Spot("pacifica",4,9,"onshore",2)
